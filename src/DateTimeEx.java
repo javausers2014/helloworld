1ed583c5ac98ef3c7b7aa71ec1397eaa3c57bbf1
mohamed <mohamed.a@y3technologies.com>
2015-10-28 17:14:45 +0800

    commit for DateTime

---
+++ src/DateTimeEx.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+/**
+ * Created by y3user on 28/10/15.
+ */
+public class DateTimeEx {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+
+}
